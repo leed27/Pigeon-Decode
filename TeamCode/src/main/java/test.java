4d038d03d5773e20ceb5b1aa39e3abec7eac18a0
Adhithya Yuvaraj <adhyuva8@gmail.com>
2025-09-16 18:55:53 -0400

    Dylan teaches Adhithya stuff Part 1

---
+++ TeamCode/src/main/java/test.java
@@ -0,0 +1,31 @@
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name = "test")
+public class test extends LinearOpMode {
+
+    Servo servo;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        servo = hardwareMap.get(Servo.class, "servo");
+
+        waitForStart();
+        servo.setPosition(0);
+        servo.setPosition(1);
+
+        while(opModeIsActive()){
+
+            double currentPos = servo.getPosition();
+            telemetry.addLine();
+            telemetry.addData("Current position: ", currentPos);
+            telemetry.addData("Current position: ", 0);
+            telemetry.addData("Current position: ", 1);
+
+            telemetry.update();
+
+        }
+
+    }
+}
